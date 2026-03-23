cb7bc9b51fb915587792c055c7cf18ef1a99e307
Duncan Leo <hello@duncanleo.me>
2026-03-23 14:10:24 +0800

    chore: initial command scaffold

--- Sources/KShowSubCLI/KShowSubCLI.swift
+++ Sources/KShowSubCLI/KShowSubCLI.swift
@@ -1,9 +1,18 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
 
+import ArgumentParser
+import Foundation
+
 @main
-struct KShowSubCLI {
-    static func main() {
+struct KShowSubCLI: ParsableCommand {
+    static let configuration = CommandConfiguration(
+        commandName: ProcessInfo.processInfo.processName)
+
+    @Option(help: "Specify the input")
+    public var input: String
+
+    public func run() throws {
         print("Hello, world!")
     }
 }
